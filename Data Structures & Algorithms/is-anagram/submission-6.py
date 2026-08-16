28ef3983cba51b3b5c15b69c081cf87c01e8ef3b
AMIR <112046597+iamirrf@users.noreply.github.com>
2026-08-16 11:05:34 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        lists = {}
+        listt = {}
+        for i in range(len(s)):
+            if s[i] in lists:
+                lists[s[i]] += 1
+            else:
+                lists[s[i]] = 1
+        for j in range(len(t)):
+            if t[j] in listt:
+                listt[t[j]] += 1
+            else:
+                listt[t[j]] = 1
+
+        if lists == listt:
+            return True
+        else:
+            return False
+
+        
